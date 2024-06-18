a2b7817b561663c2f0b9334fc3d7d816c3eee9e9
CWDamm <cwdamm@gmail.com>
2024-06-18 09:51:09 +0100

    add comment list functionality

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
-import { getArticles } from '../axios';
+import { getArticles, getComments } from '../axios';
+import CommentCard from './CommentCard'
 
 const ArticlePage = () => {
 
     const { article_id } = useParams();
-    const [currentArticle, setCurrentArticle] = useState({})
+    const [currentArticle, setCurrentArticle] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+
+    const [commentList, setCommentList] = useState([]);
+    const [showComments, setshowComments] = useState(false);
+
 
     useEffect(() => {
         console.log("article-effect activated")
@@ -15,7 +20,15 @@ const ArticlePage = () => {
                 setCurrentArticle(response.article)
                 setIsLoading(false)
             })
+        getComments(article_id)
+            .then(response => {
+                setCommentList(response.comments);
+            })
     }, [])
+
+    const handleOnShowCommentsClick = () => {
+        setshowComments(!showComments)
+    }
 
     const unformattedDate = new Date(currentArticle.created_at);
     const formattedDate = unformattedDate.toLocaleString('en-GB', {});
@@ -24,10 +37,22 @@ const ArticlePage = () => {
     return (
         <article id="article-page">
             <h2>{currentArticle.title}</h2>
-                <img id='article-page-img' src={currentArticle.article_img_url} alt="{currentArticle.title}"/>
-            <p>Author: {currentArticle.author}</p>
-            <p>{formattedDate}</p>
+            <img id='article-page-img' src={currentArticle.article_img_url} alt="{currentArticle.title}" />
+            <p className='article-author'>Author: {currentArticle.author}</p>
+            <p className='article-time-stamp'>{formattedDate}</p>
             <p>{currentArticle.body}</p>
+            <h3>Comments</h3>
+
+            <button id="comment-show-button" onClick={handleOnShowCommentsClick}>{!showComments ? "Show comments" : "Hide Comments"}</button>
+            {showComments ?
+                <section>
+                    <ul id="comment-container">
+                        {commentList.map((comment) => {
+                            return <CommentCard key={comment.comment_id} comment={comment} />
+                        })}
+                    </ul>
+                </section> : null
+            }
         </article>
     )
 }
